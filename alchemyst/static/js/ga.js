fec0644373a8e3864670634cc4db544e26833f84
Alex M <alex@mosstech.io>
2021-06-19 18:55:07 +0100

    Update GoogleAnalytics tracking code

--- alchemyst/static/js/ga.js
+++ alchemyst/static/js/ga.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', 'UA-86642618-1');
+gtag('config', 'G-PFDDSNZ32V');
