62294856a61fe4ca8b588694cf7c1eb15d840f6c
Alex M <alex@mosstech.io>
2023-03-18 20:29:25 +0000

    Update bootstrap script to work within migrator CI

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,19 +5,21 @@ if [[ -z $GCP_PROJECT_ID ]]; then
   read -r GCP_PROJECT_ID
 fi
 
-gcloud iam service-accounts create alchemyst \
-  --display-name=alchemyst \
-  --description="Workload Identity access to Datastore" \
-  --project=${GCP_PROJECT_ID}
+if [[ $(gcloud iam service-accounts list --project="${GCP_PROJECT_ID}" --format="value(display_name)" | grep -c "alchemyst") -eq 0 ]]; then
+  gcloud iam service-accounts create alchemyst \
+    --display-name=alchemyst \
+    --description="Workload Identity access to Datastore" \
+    --project="${GCP_PROJECT_ID}"
+fi
 
-gcloud projects add-iam-policy-binding ${GCP_PROJECT_ID} \
+gcloud projects add-iam-policy-binding "${GCP_PROJECT_ID}" \
   --member="serviceAccount:alchemyst@${GCP_PROJECT_ID}.iam.gserviceaccount.com" \
-  --role=roles/datastore.user
-  
+  --role=roles/datastore.user >/dev/null 
+
 gcloud iam service-accounts add-iam-policy-binding \
-  --project=${GCP_PROJECT_ID} \
+  --project="${GCP_PROJECT_ID}" \
   --role roles/iam.workloadIdentityUser \
   --member "serviceAccount:${GCP_PROJECT_ID}.svc.id.goog[alchemyst/alchemyst]" \
-  alchemyst@${GCP_PROJECT_ID}.iam.gserviceaccount.com
+  alchemyst@"${GCP_PROJECT_ID}".iam.gserviceaccount.com > /dev/null
 
 echo "NB: You must also enable Datastore for the project"
