7146112ca923267dc3f5f80488c8146ced14b6cc
Alex M <alex@alexos.dev>
2024-05-09 23:14:19 +0100

    Remove older attempts to convert PDFs to HTML

--- wip/convert_pdf_to_html.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pdftotree
-
-
-if __name__ == '__main__':
-    pdftotree.parse(sys.argv[1], html_path=None, model_type=None, model_path=None, favor_figures=True, visualize=False)
-
-
-# pip install pylzma
