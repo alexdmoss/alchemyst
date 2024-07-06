96a54c0b8019e4a20153dc009629c336874f943c
Alex M <alex@alexos.dev>
2024-07-06 20:48:12 +0100

    Reenable apex

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -1,18 +1,18 @@
-# resource "google_cloud_run_domain_mapping" "apex-domain" {
+resource "google_cloud_run_domain_mapping" "apex-domain" {
 
-#   name     = var.domain
-#   location = google_cloud_run_v2_service.app.location
+  name     = var.domain
+  location = google_cloud_run_v2_service.app.location
 
-#   metadata {
-#     namespace = var.gcp_project_id
-#   }
+  metadata {
+    namespace = var.gcp_project_id
+  }
 
-#   spec {
-#     route_name     = google_cloud_run_v2_service.app.name
-#     force_override = true
-#   }
+  spec {
+    route_name     = google_cloud_run_v2_service.app.name
+    force_override = true
+  }
 
-# }
+}
 
 resource "google_cloud_run_domain_mapping" "www-domain" {
 
