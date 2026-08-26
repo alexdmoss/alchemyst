0101eab67b41b3ce80f21b8e5b429f4613e40ad3
Alex M <alex@alexos.dev>
2026-08-26 12:33:08 +0100

    Fix: flask-caching behaviour changed on update

--- alchemyst/cache.py
+++ alchemyst/cache.py
@@ -4,7 +4,7 @@ from flask_caching import Cache
 
 DEBUG_MODE = getenv("FLASK_DEBUG", "false").lower() in {"1", "t", "true"}
 
-cache_type = "null" if DEBUG_MODE else "simple"
+cache_type = "NullCache" if DEBUG_MODE else "SimpleCache"
 cache_config = {
     "CACHE_TYPE": cache_type,
     "CACHE_DEFAULT_TIMEOUT": 0,  # infinite cache by default, as the data is very static
